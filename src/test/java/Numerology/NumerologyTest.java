1692fbe91cf0ccb09263e94b515208fc1ef598fc
DESKTOP-35G9JNR\eugenia <melopez@aubay.es>
2020-09-04 19:11:55 +0200

    test testReplaceEachNineForTwoTens passed.

--- src/test/java/Numerology/NumerologyTest.java
+++ src/test/java/Numerology/NumerologyTest.java
@@ -3,20 +3,35 @@
  */
 package Numerology;
 
-import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 import org.junit.jupiter.api.Test;
 
+
 public class NumerologyTest {
     @Test 
-    public void testSomeLibraryMethod() {
-        Numerology classUnderTest = new Numerology();
-        assertTrue(classUnderTest.someLibraryMethod(), "someLibraryMethod should return 'true'");
+    public void testReplaceEachNineForTwoTens() {
+        Numerology numerology = new Numerology();
+        assertEquals(Arrays.asList(1,2,3,4,5,6,7,8,10,10,10), numerology.replaceEachNineForTwoTens(Arrays.asList(1,2,3,4,5,6,7,8,9,10)));
     }
     
     public class Numerology {
-    	public boolean someLibraryMethod() {
-            return true;
+    	public List<Integer> replaceEachNineForTwoTens(List<Integer> in) {
+            //return in.stream().map(o -> o == 9 ? 1 : o).collect(Collectors.toList());
+    		List<Integer> out = new ArrayList<Integer>();
+    		for (int i = 0; i< in.size(); i++) {
+    			if (in.get(i).equals(9)) {
+    				out.add(10);
+    				out.add(10);
+    			} else {
+    				out.add(in.get(i));
+    			}
+    		}
+    		return out;
         }
     }
 }
